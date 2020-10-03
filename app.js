0eace39bed237390c8e15eedeae591a9653bf6d1
TimothyWang56 <timothywang56@gmail.com>
2020-10-03 10:37:57 -0400

    add set interval (for later use)

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var app = express();
+var serv = require("http").Server(app);
+
+serv.listen(process.env.PORT || 2000);
+console.log("server started");
+
+SOCKET_LIST = {};
+
+var io = require("socket.io")(serv,{});
+io.sockets.on("connection", function(socket) {
+    console.log("connection detected");
+    socket.id = Math.random();
+    SOCKET_LIST[socket.id] = socket;
+
+    socket.on("disconnect", function() {
+        delete SOCKET_LIST[socket.id];
+        console.log("connection deleted");
+    });
+
+    socket.on("surveyComplete", function(data) {
+        console.log("survey data received");
+        // store survey data and find match?
+    });
+})
+
+setInterval(function() {
+
+}, 1000)
