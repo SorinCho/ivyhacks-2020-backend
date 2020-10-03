e577031907977246d06d9e593014d09a15e3ea60
TimothyWang56 <timothywang56@gmail.com>
2020-10-03 17:57:14 -0400

    remove new change

--- app.js
+++ app.js
@@ -13,18 +13,7 @@ SOCKET_LIST = {};
 USER_RESPONSES = {};
 UNMATCHED_USERS = {};
 
-const io = require("socket.io")(server, {
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
-        }
-        res.writeHead(200, headers);
-        res.end();
-    }
-});
-
+const io = require("socket.io")(server);
 io.on("connection", function(socket) {
     console.log("connection detected");
     socket.id = makeId(10);
