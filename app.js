6dafe115de11f191b0b93e942ef33aaa065c1e28
SorinCho <sorincho@gmail.com>
2020-10-03 18:22:51 -0400

    print port and test

--- app.js
+++ app.js
@@ -6,101 +6,104 @@ app.use(cors());
 
 const rooms = require("./src/rooms");
 
-const server = require("http").createServer(app);
+// const server = require("http").createServer(app);
 
 SOCKET_LIST = {};
 
 USER_RESPONSES = {};
 UNMATCHED_USERS = {};
 
-const io = require("socket.io")(server);
-io.on("connection", function(socket) {
-    console.log("connection detected");
-    socket.id = makeId(10);
-    SOCKET_LIST[socket.id] = socket;
+const io = require("socket.io").listen(app);
+io.on("connection", function (socket) {
+  console.log("connection detected");
+  socket.id = makeId(10);
+  SOCKET_LIST[socket.id] = socket;
 
-    socket.on("disconnect", function() {
-        delete SOCKET_LIST[socket.id];
-        delete USER_RESPONSES[socket.id];
-        delete UNMATCHED_USERS[socket.id];
-        for (let room in ROOMS) {
-            if (ROOMS[room].includes(socket.id)) {
-                ROOMS[room] = ROOMS[room].filter(id => id !== socket.id);
-                break;
-            }
-        }
-        console.log("connection deleted");
-    });
+  socket.on("disconnect", function () {
+    delete SOCKET_LIST[socket.id];
+    delete USER_RESPONSES[socket.id];
+    delete UNMATCHED_USERS[socket.id];
+    for (let room in ROOMS) {
+      if (ROOMS[room].includes(socket.id)) {
+        ROOMS[room] = ROOMS[room].filter((id) => id !== socket.id);
+        break;
+      }
+    }
+    console.log("connection deleted");
+  });
 
-    // socket.on("surveyComplete", function(data) {
-    //     console.log("survey data received");
-    //     // store survey data and find match?
-    //     USER_RESPONSES[socket.id] = {};
-    //     UNMATCHED_USERS[socket.id] = 1;
-    //     startMatch(socket.id);
-    // });
-    USER_RESPONSES[socket.id] = {};
-    UNMATCHED_USERS[socket.id] = 1;
-    startMatch(socket.id);
-})
+  // socket.on("surveyComplete", function(data) {
+  //     console.log("survey data received");
+  //     // store survey data and find match?
+  //     USER_RESPONSES[socket.id] = {};
+  //     UNMATCHED_USERS[socket.id] = 1;
+  //     startMatch(socket.id);
+  // });
+  USER_RESPONSES[socket.id] = {};
+  UNMATCHED_USERS[socket.id] = 1;
+  startMatch(socket.id);
+});
 
 server.listen(process.env.PORT || 2000);
+var port = process.env.PORT || 2000;
+console.log(port);
 console.log("server started");
 
 function isAMatch(res1, res2) {
-    return true;
+  return true;
 }
 
 function startMatch(socketId) {
-    console.log("starting match");
-    const userResponse = USER_RESPONSES[socketId];
+  console.log("starting match");
+  const userResponse = USER_RESPONSES[socketId];
 
-    for (let id in UNMATCHED_USERS) {
-        if (id !== socketId) {
-            otherUserResponse = USER_RESPONSES[id];
-            if (isAMatch(userResponse, otherUserResponse)) {
-                const url = putInRoom(socketId, id);
-                console.log(url);
-                SOCKET_LIST[socketId].emit("matched", url);
-                SOCKET_LIST[id].emit("matched", url);
-                return;
-            }
-        }
+  for (let id in UNMATCHED_USERS) {
+    if (id !== socketId) {
+      otherUserResponse = USER_RESPONSES[id];
+      if (isAMatch(userResponse, otherUserResponse)) {
+        const url = putInRoom(socketId, id);
+        console.log(url);
+        SOCKET_LIST[socketId].emit("matched", url);
+        SOCKET_LIST[id].emit("matched", url);
+        return;
+      }
     }
+  }
 
-    // what now? maybe have background process try to continuously match.
+  // what now? maybe have background process try to continuously match.
 }
 
 // roomId to list of participants in room
 ROOMS = {};
 
 function putInRoom(socketId1, socketId2) {
-    for (let roomId in ROOMS) {
-        if (ROOMS[roomId].length === 0) {
-            ROOMS[roomId] = [socketId1, socketId2];
-            delete UNMATCHED_USERS[socketId1];
-            delete UNMATCHED_USERS[socketId2];
-            return rooms.DAILYCOURL + roomId;
-        }
+  for (let roomId in ROOMS) {
+    if (ROOMS[roomId].length === 0) {
+      ROOMS[roomId] = [socketId1, socketId2];
+      delete UNMATCHED_USERS[socketId1];
+      delete UNMATCHED_USERS[socketId2];
+      return rooms.DAILYCOURL + roomId;
     }
+  }
 
-    const newRoomId = makeId(10);
+  const newRoomId = makeId(10);
 
-    if (rooms.createRoom(newRoomId)) {
-        ROOMS[newRoomId] = [socketId1, socketId2];
-        delete UNMATCHED_USERS[socketId1];
-        delete UNMATCHED_USERS[socketId2];
-        return rooms.DAILYCOURL + newRoomId;
-    }
+  if (rooms.createRoom(newRoomId)) {
+    ROOMS[newRoomId] = [socketId1, socketId2];
+    delete UNMATCHED_USERS[socketId1];
+    delete UNMATCHED_USERS[socketId2];
+    return rooms.DAILYCOURL + newRoomId;
+  }
 }
 
 function makeId(length) {
-    var result = "";
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
+  var result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
 
-    return result;
- }
+  return result;
+}
