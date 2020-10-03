c53234f233db2f77e3918d39cf1c2a5d77d55c63
SorinCho <sorincho@gmail.com>
2020-10-03 11:17:15 -0400

    mongoose

--- app.js
+++ app.js
@@ -2,9 +2,33 @@ var express = require("express");
 var app = express();
 var serv = require("http").Server(app);
 var axios = require("axios");
+var mongoose = require('mongoose');
 
 serv.listen(process.env.PORT || 2000);
 console.log("server started");
+
+var Message = mongoose.model('Message',{
+    name : String,
+    message : String
+  })
+
+const uri = "mongodb+srv://new-user:Q8B7o3zYDlWdEFfV@cluster0.6mcs4.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+app.get('/messages', (req, res) => {
+    Message.find({},(err, messages)=> {
+      res.send(messages);
+    })
+  })
+  
+  app.post('/messages', (req, res) => {
+    var message = new Message(req.body);
+    message.save((err) =>{
+      if(err)
+        sendStatus(500);
+      io.emit('message', req.body);
+      res.sendStatus(200);
+    })
+  })
 
 SOCKET_LIST = {};
 
@@ -57,3 +81,12 @@ async function createRoom(roomName) {
 setInterval(function() {
 
 }, 1000)
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('MongoDB Connected...')
+  })
+  .catch(err => console.log(err))
